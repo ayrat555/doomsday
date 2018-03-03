67c6b7cbc1aaedfc7d0141d38a6fa90abb5e7aa3
Ayrat Badykov <ayratin555@gmail.com>
2018-03-03 23:28:41 +0300

    brand new start

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,5 @@
-extern crate chrono;
+extern crate cronenberg;
 extern crate notify_rust;
 
-mod org_parser;
-mod notification;
-use schedule::Agenda;
-
 fn main() {
-    let mut a = Agenda::new();
-
-    a.add(|| {
-        Notification::new()
-            .summary("Doomsday")
-            .body("Hey")
-            .show().unwrap();
-    }).schedule("* * * * * *").unwrap();
 }
